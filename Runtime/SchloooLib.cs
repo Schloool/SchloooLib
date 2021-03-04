72a9408d98ed89a87c201e32256297d962436026
Max Schlosser <kontakt@max-schlosser.de>
2021-03-04 17:26:44 +0100

    Add initial test structure

---
+++ Runtime/SchloooLib.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SchloooLib.Core
+{
+    public class SchloooLib : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("This GameObject is using a SchloooLib example script.");
+        }
+    }
+}
+
