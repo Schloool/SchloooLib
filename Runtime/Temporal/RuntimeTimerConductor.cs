0721957294e87abccdfe32aecbd5ddc1fa014d82
Max Schlosser <kontakt@max-schlosser.de>
2021-03-05 20:33:56 +0100

    Add primitive timer util class

---
+++ Runtime/Temporal/RuntimeTimerConductor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using SchloooLib.Core;
+
+namespace SchloooLib.Temporal
+{
+    public sealed class RuntimeTimerConductor : Singleton<RuntimeTimerConductor>
+    {
+        private List<Timer> timers = new List<Timer>();
+        private List<Timer> timerBacklog = new List<Timer>();
+
+        private void Update()
+        {
+            ProgressBacklog();
+            TickTimers();
+        }
+        
+        private void ProgressBacklog()
+        {
+            if (timerBacklog.Count <= 0) return;
+            
+            timerBacklog.ForEach(backlogTimer => timers.Add(backlogTimer));
+            timerBacklog.Clear();
+        }
+
+        private void TickTimers()
+        {
+            timers.ForEach(timer => timer.Tick());
+            timers.RemoveAll(timer => timer.IsFinished);
+        }
+
+        public void AddTimer(Timer timer)
+        {
+            timerBacklog.Add(timer);
+        }
+
+        public void StopAllTimers()
+        {
+            timers.ForEach(timer => timer.Stop());
+            timers.Clear();
+        }
+    }
+}
---
+++ Runtime/Temporal/Timer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UnityEngine;
+
+namespace SchloooLib.Temporal
+{
+    public class Timer
+    {
+        public float Duration { get; }
+        public bool IsLooping { get; }
+        public bool UsesGameTimescale { get; }
+        public bool IsFinished { get; private set; }
+        public bool IsPaused { get; private set; } 
+
+        private Action onFinish;
+        private Action<float> onTick;
+        private float startupTime;
+        private float lastUpdateTime;
+        
+        private float? elapsedTimeBeforePause;
+
+        public Timer(float duration, Action onFinish, Action<float> onTick = null, bool isLooping = false, bool usesGameTimescale = false, bool runInstantly = true)
+        {
+            Duration = duration;
+            this.onFinish = onFinish;
+            this.onTick = onTick;
+            IsLooping = isLooping;
+            UsesGameTimescale = usesGameTimescale;
+            IsFinished = false;
+
+            IsPaused = !runInstantly;
+            RuntimeTimerConductor.Instance.AddTimer(this);
+        }
+        
+        public void Tick()
+        {
+            if (IsFinished) return;
+
+            if (IsPaused)
+            {
+                ProgressPauseState();
+                return;
+            }
+
+            lastUpdateTime = GetGlobalTime();
+            onTick?.Invoke(GetTimeSinceTimerStart());
+
+            if (GetGlobalTime() >= GetFinishTime())
+            {
+                ProgressCycleFinish();
+            }
+        }
+
+        private void ProgressPauseState()
+        {
+            startupTime += GetTickDeltaTime();
+            lastUpdateTime = GetGlobalTime();
+        }
+        
+        private float GetTickDeltaTime()
+        {
+            return GetGlobalTime() - lastUpdateTime;
+        }
+
+        private float GetGlobalTime()
+        {
+            return UsesGameTimescale ? Time.time : Time.realtimeSinceStartup;
+        }
+
+        private float GetTimeSinceTimerStart()
+        {
+            if (IsFinished || GetGlobalTime() >= GetFinishTime()) return Duration;
+            
+            return elapsedTimeBeforePause ?? GetGlobalTime() - startupTime;
+        }
+        
+        private float GetFinishTime()
+        {
+            return startupTime + Duration;
+        }
+
+        private void ProgressCycleFinish()
+        {
+            onFinish?.Invoke();
+            
+            if (IsLooping)
+            {
+                startupTime = GetGlobalTime();
+            }
+            else
+            {
+                IsFinished = true;
+            }
+        }
+        
+        public void Pause()
+        {
+            if (IsPaused || IsFinished) return;
+
+            elapsedTimeBeforePause = GetTimeSinceTimerStart();
+            IsPaused = true;
+        }
+
+        public void Unpause()
+        {
+            if (!IsPaused || IsFinished) return;
+
+            elapsedTimeBeforePause = null;
+            IsPaused = false;
+        }
+
+        public void Stop()
+        {
+            if (IsFinished) return;
+
+            IsFinished = true;
+        }
+    }
+}
