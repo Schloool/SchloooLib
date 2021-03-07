370edbff9c0d475ec40fd3b23d99e44417844980
Max Schlosser <kontakt@max-schlosser.de>
2021-03-07 18:22:52 +0100

    Add docs

--- Runtime/Temporal/RuntimeTimerConductor.cs
+++ Runtime/Temporal/RuntimeTimerConductor.cs
@@ -3,6 +3,9 @@ using SchloooLib.Core;
 
 namespace SchloooLib.Temporal
 {
+    /// <summary>
+    /// Central controller to register and update <see cref="Timer"/> instances.
+    /// </summary>
     public sealed class RuntimeTimerConductor : Singleton<RuntimeTimerConductor>
     {
         private List<Timer> timers = new List<Timer>();
@@ -28,11 +31,18 @@ namespace SchloooLib.Temporal
             timers.RemoveAll(timer => timer.IsFinished);
         }
 
+        /// <summary>
+        /// Registers a new <see cref="Timer"/> that will be registered to be updated.
+        /// </summary>
+        /// <param name="timer">The new <see cref="Timer"/> instance.</param>
         public void AddTimer(Timer timer)
         {
             timerBacklog.Add(timer);
         }
 
+        /// <summary>
+        /// Stops all <see cref="Timer"/>s controlled by this <see cref="RuntimeTimerConductor"/> instance.
+        /// </summary>
         public void StopAllTimers()
         {
             timers.ForEach(timer => timer.Stop());
--- Runtime/Temporal/Timer.cs
+++ Runtime/Temporal/Timer.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 
 namespace SchloooLib.Temporal
 {
+    /// <summary>
+    /// Main class to conduct delayed or time-based looped activities.
+    /// </summary>
     public class Timer
     {
+        /// <summary>
+        /// The amount of seconds it will take to run one cycle.
+        /// </summary>
         public float Duration { get; }
+        /// <summary>
+        /// Indicates whether the instance will loop or not.
+        /// </summary>
         public bool IsLooping { get; }
+        /// <summary>
+        /// Indicates whether the instance uses the Unity timescale or real time.
+        /// </summary>
         public bool UsesGameTimescale { get; }
+        /// <summary>
+        /// Indicates whether the instance has already finished or not.
+        /// A <see cref="Timer"/> is set as finished when running through a not looping delay or when stopped manually.
+        /// </summary>
         public bool IsFinished { get; private set; }
+        /// <summary>
+        /// Indicates if the instance is currently paused.
+        /// The pause flag gets also set when the <see cref="Timer"/> is not run instantly.
+        /// </summary>
         public bool IsPaused { get; private set; } 
 
         private Action onFinish;
@@ -18,11 +38,30 @@ namespace SchloooLib.Temporal
         
         private float? elapsedTimeBeforePause;
 
+        /// <summary>
+        /// Constructs a new <c>Timer</c> instance running as an time-delayed loop.
+        /// </summary>
+        /// <param name="delayPerIteration">The amount of seconds one loop iteration will last.</param>
+        /// <param name="onIterationFinish"><see cref="Action"/> delegate called whenever one iteration has finished.</param>
+        /// <param name="onTick"><see cref="Action"/> called whenever the <see cref="RuntimeTimerConductor"/> casts an update cycle.</param>
+        /// <param name="usesGameTimescale">Adjusts whether the Unity timescale will influence time measurement or not.</param>
+        /// <param name="runInstantly">Adjusts whether the delay will run instantly or will be created in a paused state.</param>
+        /// <returns>The resulting <c>Timer</c> instance.</returns>
         public static Timer ScheduleLoop(float delayPerIteration, Action onIterationFinish, Action<float> onTick = null, bool usesGameTimescale = false, bool runInstantly = true)
         {
             return new Timer(delayPerIteration, onIterationFinish, onTick, true, usesGameTimescale, runInstantly);
         }
         
+        /// <summary>
+        /// Constructs a new <c>Timer</c> instance running as an one-time delay.
+        /// </summary>
+        /// <param name="delayDuration">The amount of seconds the delay will last.</param>
+        /// <param name="onFinish"><see cref="Action"/> delegate called whenever one iteration has finished.</param>
+        /// <param name="onTick"><see cref="Action"/> called whenever the <see cref="RuntimeTimerConductor"/> casts an update cycle.</param>
+        /// <param name="usesGameTimescale">Adjusts whether the Unity timescale will influence time measurement or not.</param>
+        /// <param name="runInstantly">Adjusts whether the delay will run instantly or will be created in a paused state.</param>
+        /// <returns>The resulting <c>Timer</c> instance.</returns>
+        /// <returns></returns>
         public static Timer ScheduleDelay(float delayDuration, Action onFinish, Action<float> onTick = null, bool usesGameTimescale = false, bool runInstantly = true)
         {
             return new Timer(delayDuration, onFinish, onTick, false, usesGameTimescale, runInstantly);
@@ -41,6 +80,9 @@ namespace SchloooLib.Temporal
             RuntimeTimerConductor.Instance.AddTimer(this);
         }
         
+        /// <summary>
+        /// Processes the time variables differentiating between running, paused and finished states.
+        /// </summary>
         public void Tick()
         {
             if (IsFinished) return;
@@ -58,8 +100,9 @@ namespace SchloooLib.Temporal
             {
                 ProgressCycleFinish();
             }
+            
         }
-
+        
         private void ProgressPauseState()
         {
             startupTime += GetTickDeltaTime();
@@ -70,12 +113,16 @@ namespace SchloooLib.Temporal
         {
             return GetGlobalTime() - lastUpdateTime;
         }
-
+        
         private float GetGlobalTime()
         {
             return UsesGameTimescale ? Time.time : Time.realtimeSinceStartup;
         }
 
+        /// <summary>
+        /// Calculates the time that has passed since the current cycle has started.
+        /// </summary>
+        /// <returns></returns>
         public float GetTimeSinceTimerStart()
         {
             if (IsFinished || GetGlobalTime() >= GetFinishTime()) return Duration;
@@ -102,6 +149,10 @@ namespace SchloooLib.Temporal
             }
         }
         
+        /// <summary>
+        /// Pauses the <see cref="Timer"/>.
+        /// Does nothing when the instance is paused already or has finished.
+        /// </summary>
         public void Pause()
         {
             if (IsPaused || IsFinished) return;
@@ -110,6 +161,10 @@ namespace SchloooLib.Temporal
             IsPaused = true;
         }
 
+        /// <summary>
+        /// Unpauses the <see cref="Timer"/>.
+        /// Does nothing when the instance is unpaused already or has finished.
+        /// </summary>
         public void Unpause()
         {
             if (!IsPaused || IsFinished) return;
@@ -118,6 +173,10 @@ namespace SchloooLib.Temporal
             IsPaused = false;
         }
 
+        /// <summary>
+        /// Stops the <see cref="Timer"/>.
+        /// A stopped instance can not be resumed later.
+        /// </summary>
         public void Stop()
         {
             if (IsFinished) return;
