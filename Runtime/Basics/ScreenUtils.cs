0a5a600984a89996a3297ee81b80404b81a0800f
Max Schlosser <kontakt@max-schlosser.de>
2021-03-21 17:23:41 +0100

    Add basic screen utilities

---
+++ Runtime/Basics/ScreenUtils.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace SchloooLib.Core
+{
+    public class ScreenUtils
+    {
+        public static float GetTopWorldBorder(Camera camera)
+        {
+            return camera.ViewportToWorldPoint(Vector2.one).y;
+        }
+
+        public static float GetBottomWorldBorder(Camera camera)
+        {
+            return camera.ViewportToWorldPoint(Vector2.zero).y;
+        }
+
+        public static float GetLeftWorldBorder(Camera camera)
+        {
+            return camera.ViewportToWorldPoint(Vector2.zero).x;
+        }
+        
+        public static float GetRightWorldBorder(Camera camera)
+        {
+            return camera.ViewportToWorldPoint(Vector2.one).x;
+        }
+
+        public static Vector2 GetWorldMiddlePoint(Camera camera)
+        {
+            return camera.ViewportToWorldPoint(Vector2.one * 0.5f);
+        }
+
+        public static float GetVerticalWorldMiddle(Camera camera)
+        {
+            return GetWorldMiddlePoint(camera).y;
+        }
+        
+        public static float GetHorizontalWorldMiddle(Camera camera)
+        {
+            return GetWorldMiddlePoint(camera).x;
+        }
+    }
+}
---
+++ Tests/Runtime/ScreenUtilTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using SchloooLib.Core;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+
+namespace SchloooLib.Tests
+{
+    public class ScreenUtilTests
+    {
+        private readonly Camera camera;
+
+        public ScreenUtilTests()
+        {
+            camera = new GameObject().AddComponent<Camera>();
+            camera.orthographic = true;
+        }
+
+        [UnityTest]
+        public IEnumerator Top_World_Border_Is_Bigger_Than_Bottom()
+        {
+            float topBorder = ScreenUtils.GetTopWorldBorder(camera);
+            float bottomBorder = ScreenUtils.GetBottomWorldBorder(camera);
+
+            Assert.IsTrue(topBorder >= bottomBorder);
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator Right_World_Border_Is_Bigger_Than_Left()
+        {
+            float leftBorder = ScreenUtils.GetLeftWorldBorder(camera);
+            float rightBorder = ScreenUtils.GetRightWorldBorder(camera);
+            
+            Assert.IsTrue(rightBorder >= leftBorder);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator World_Middle_Point_Matches_Camera_Transform()
+        {
+            Vector2 worldMiddlePoint = ScreenUtils.GetWorldMiddlePoint(camera);
+            Vector2 cameraPosition = camera.transform.position;
+            
+            Assert.AreApproximatelyEqual(cameraPosition.x, worldMiddlePoint.x);
+            Assert.AreApproximatelyEqual(cameraPosition.y, worldMiddlePoint.y);
+            yield return null;
+        }
+    }
+}
